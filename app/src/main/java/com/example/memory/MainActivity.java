88e7fd0916365d8cfacbd645e648393b8b8333db
Nicola Gassmann <Nicola.Gassmann@gmail.com>
2019-10-24 13:50:32 +0200

    Copied given code

--- app/src/main/java/com/example/memory/MainActivity.java
+++ app/src/main/java/com/example/memory/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.memory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void takeQrCodePicture() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(MyCaptureActivity.class);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPE);
+        integrator.setOrientationLocked(false);
+        integrator.addExtra(Intents.Scan.BARCODE_IMAGE_ENABLED, true);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == IntentIntegrator.REQUEST_CODE
+                && resultCode == RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            String path = extras.getString(
+                    Intents.Scan.RESULT_BARCODE_IMAGE_PATH);
+
+            // Ein Bitmap zur Darstellung erhalten wir so:
+            // Bitmap bmp = BitmapFactory.decodeFile(path)
+
+            String code = extras.getString(
+                    Intents.Scan.RESULT);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/memory/MyCaptureActivity.java
@@ -0,0 +1,5 @@
+package com.example.memory;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class MyCaptureActivity extends CaptureActivity { }
